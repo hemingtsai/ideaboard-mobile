9541b16850ea4bdaf2e10d1f27a512bd551cc169
hemingtsai <hemingtsai@hotmail.com>
2026-06-12 16:37:37 +0800

    为新加入的页面添加路由

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,18 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import HomeView from "./views/HomeView.vue";
+import ProjectsListView from "./views/ProjectsListView.vue";
+import SettingsView from "./views/SettingsView.vue";
+import ProjectDetailsView from "./views/ProjectDetailsView.vue";
 
 const routes = [
-  { path: '/', component: HomeView }
-]
+  { path: "/", component: HomeView },
+  { path: "/projects", component: ProjectsListView },
+  { path: "/settings", component: SettingsView },
+  { path: "/projects/:id", component: ProjectDetailsView },
+];
 
 export const router = createRouter({
   history: createMemoryHistory(),
-  routes
-})
+  routes,
+});
