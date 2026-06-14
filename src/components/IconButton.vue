31e03f0d6703aeae82f38488ddfb337642f575d2
hemingtsai <hemingtsai@hotmail.com>
2026-06-14 13:19:13 +0800

    修改图标按钮设计

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -1,27 +1,59 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { Icon } from "@iconify/vue";
 
 defineEmits<{
     click: [];
 }>();
 
-const props = defineProps({
-    icon: {
-        type: String,
-        default: "material-symbols-light:10k-sharp",
+const props = withDefaults(
+    defineProps<{
+        /** Iconify 图标名 */
+        icon?: string;
+        /** 圆形按钮直径 (px)，同时控制图标大小 */
+        size?: number;
+        /** 背景颜色 */
+        color?: string;
+        /** 图标颜色 */
+        textColor?: string;
+    }>(),
+    {
+        icon: "material-symbols-light:10k-sharp",
+        size: 36,
+        color: "#FFFFFF",
+        textColor: "black",
     },
-});
+);
+
+const containerStyle = computed(() => ({
+    width: props.size + "px",
+    height: props.size + "px",
+    backgroundColor: props.color,
+}));
+
+const iconStyle = computed(() => ({
+    fontSize: props.size * 0.47 + "pt",
+    color: props.textColor,
+}));
 </script>
 
 <template>
-    <div class="icon-container" @click="$emit('click')">
-        <Icon :icon="props.icon" class="icon"></Icon>
+    <div class="icon-container" :style="containerStyle" @click="$emit('click')">
+        <Icon :icon="props.icon" class="icon" :style="iconStyle"></Icon>
     </div>
 </template>
 
 <style scoped>
+.icon-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    flex-shrink: 0;
+    -webkit-tap-highlight-color: transparent;
+}
+
 .icon {
     user-select: none;
-    font-size: 17pt;
 }
 </style>
