f10e51fbc8280c90a712170d91b1f6aa3e107b65
hemingtsai <hemingtsai@hotmail.com>
2026-07-04 14:58:20 +0800

    防止误选中文字

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import { router } from "./router.ts";
 import { i18n } from "./i18n.ts";
 import "./theme.css";
+import "./main.css";
 
 createApp(App).use(router).use(i18n).mount("#app");
